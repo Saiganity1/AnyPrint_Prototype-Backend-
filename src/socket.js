b66a041bc1cac788dfd17da3808ea7190891ec6e
Saiganity1 <sicatmichaeldave0411@gmail.com>
2026-04-30 11:12:44 +0800

    fix(socket.io): enable CORS for all allowed origins and add proper logging

--- src/socket.js
+++ src/socket.js
@@ -1,27 +1,71 @@
 let io = null;
 
+const isAllowedOrigin = (origin) => {
+  if (!origin) return true;
+
+  if (/^https:\/\/.*\.onrender\.com$/i.test(origin)) {
+    return true;
+  }
+
+  if (/^https:\/\/.*\.netlify\.app$/i.test(origin)) {
+    return true;
+  }
+
+  if (/^https:\/\/.*\.vercel\.app$/i.test(origin)) {
+    return true;
+  }
+
+  if (/^https?:\/\/(localhost|127\.0\.0\.1)(:\d+)?$/i.test(origin)) {
+    return true;
+  }
+
+  const allowedOrigins = [
+    process.env.CLIENT_ORIGIN,
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'http://127.0.0.1:5173',
+    'http://127.0.0.1:3000',
+  ];
+
+  return allowedOrigins.some((allowed) => allowed && allowed === origin);
+};
+
 module.exports = {
   init(server) {
     // lazy require to avoid circular imports during startup
     const { Server } = require('socket.io');
     io = new Server(server, {
       cors: {
-        origin: [process.env.CLIENT_ORIGIN, 'http://localhost:5173', 'http://localhost:3000'],
+        origin: isAllowedOrigin,
         methods: ['GET', 'POST'],
         credentials: true,
       },
     });
 
     io.on('connection', (socket) => {
+      console.log(`Socket.IO client connected: ${socket.id}`);
+      
       socket.on('join', (room) => {
-        if (room) socket.join(room);
+        if (room) {
+          socket.join(room);
+          console.log(`Socket ${socket.id} joined room: ${room}`);
+        }
       });
 
       socket.on('leave', (room) => {
-        if (room) socket.leave(room);
+        if (room) {
+          socket.leave(room);
+          console.log(`Socket ${socket.id} left room: ${room}`);
+        }
       });
 
-      socket.on('disconnect', () => {});
+      socket.on('disconnect', () => {
+        console.log(`Socket.IO client disconnected: ${socket.id}`);
+      });
+
+      socket.on('error', (err) => {
+        console.error(`Socket.IO error for ${socket.id}:`, err);
+      });
     });
 
     return io;
